55e6581decc0ddc1929db2e7a333241c1e94528b
aa-ryan <aryansinha6363@gmail.com>
2023-04-10 09:52:29 +0530

    preporcess dataset updated

--- RTCDA/preprocessing/preprocess_data.py
+++ RTCDA/preprocessing/preprocess_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 if __name__ == "__main__":
-    df = pd.read_csv("data/raw_data/2015_2_clickstream.tsv", sep='\t', header=0)
+    df = pd.read_csv("data/raw_data/2017_2_clickstream.tsv", sep='\t', header=0)
     df = df[['prev_title', 'curr_title', 'n', 'type']]
     df = df.sort_values(by='n', ascending=False)[:1000]
     df.to_csv('data/preprocessed_data.csv', index=False)
